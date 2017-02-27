8abe2e48277609ad4df49bdb1eaaa4532d7312d5
fergmac <f.macconnell@gmail.com>
2017-02-27 14:50:29 -0800

    refactor Main into statelss functional component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,18 +1,17 @@
 var React = require('react');
 var Nav = require('Nav');
 
-var Main = React.createClass({
-    render: function() {
-        return(
-            <div>
-                <Nav />
-                <h2>Main Component</h2>
-                {/* we use this anywhere we want to put the children */}
-                {this.props.children}
-            </div>
-        );
-    }
-
-});
+var Main = (props) => {
+    return(
+        <div>
+            <Nav />
+            <h2>Main Component</h2>
+            {/* we use this anywhere we want to put the children, can remove the this. 
+            when using stateless functional component with props already passed Main
+            */}
+            {props.children}
+        </div>
+    )
+};
 
 module.exports = Main;
