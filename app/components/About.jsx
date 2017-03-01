641f90c22f56a0d7fa5eec43d33ac2ca3267c2cd
fergmac <f.macconnell@gmail.com>
2017-02-28 20:24:42 -0800

    style About component and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,8 +1,23 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
     return (
-        <h3>About Component</h3>
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>This is a React Weather app I built using:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - Javascript framework.
+                </li>
+                <li>
+                    <a href="https://openweathermap.org">OpenWeatherMap</a> - Used to search for weather data by city.
+                </li>
+                <li>
+                    <a href="https://github.com/fergmac/practice-ReactWeather">GitHub</a> - My React Weather App repo.
+                </li>
+            </ul>
+        </div>
     )
 };
 
